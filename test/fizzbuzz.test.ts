c26dfa3b3e134d1aa26433a97efbdfad45bb6a7f
ben.freshwater <ben.freshwater@cazoo.co.uk>
2021-11-30 16:05:54 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -1,4 +1,5 @@
 const fizzBuzz = (input: number): string => {
+  if(input % 3 === 0 && input % 5 === 0 ) return 'fizzbuzz'
   return input % 3 === 0 ? "fizz" : "buzz"
 }
 
@@ -14,7 +15,14 @@ describe("Fizz Buzz", () => {
     expect(response).toBe("buzz")
   })
   it('Should print fizzbuzz if multiple of 3 and 5', () => {
-    const response = fizzBuzz(5)
+    const response = fizzBuzz(15)
     expect(response).toBe("fizzbuzz")
   })
 });
+
+describe('Listed fizz buzz', () => {
+  it('should return a list of fizz and buzzes given a list of numbers', () => {
+    const response = fizzBuzz([1,2,3,4,5,6,7,8])
+    expect(response).toBe(['1','2','fizz','4','buzz','fizz','7','8'])
+  })
+})
